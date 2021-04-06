eb2f3c0a3ac6a0b00249a5cb0b79a0dec0b41062
unknown <victoryahagui@gmail.com>
2021-04-06 17:59:04 -0300

    Style implement

--- laravel01/resources/views/car/index.blade.php
+++ laravel01/resources/views/car/index.blade.php
@@ -4,46 +4,63 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>AutoPeças</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
-    <body>
+    <body class="container">
         <form method="POST" action="/car">
             @csrf
             <div >
-                <div>
-                    <label for="mark" class="">Marca:</label>
-                    <input type="text" id="mark" name="mark" value="{{$car->mark}}">
+                <div class="form-group row mt-2">
+                    <label for="mark" class="col-sm-2 col-form-label" >Marca:</label>
+                    <div class="col-sm-5">                        
+                        <input type="text" class="form-control" id="mark" placeholder="Ex: Jaguar" name="mark" value="{{$car->mark}}">
+                    </div>
                 </div>
-                <div>
-                    <label for="vehicleModel">Modelo do Veiculo:</label>
-                    <input type="text" id="vehicleModel" name="vehicleModel" value="{{$car->vehicleModel}}">
+                <div class="form-group row mt-2">
+                    <label for="vehicleModel" class="col-sm-2 col-form-label" >Modelo do Veiculo:</label>
+                    <div class="col-sm-5">
+                        <input type="text" class="form-control" id="vehicleModel" placeholder="Ex: Jaguar XF" name="vehicleModel" value="{{$car->vehicleModel}}">
+                    </div>
                 </div>
-                <div>
-                    <label for="plate">Placa:</label>
-                    <input type="text" id="plate" name="plate" value="{{$car->plate}}">
+                <div class="form-group row mt-2">
+                    <label for="plate" class="col-sm-2 col-form-label" >Placa:</label>
+                    <div class="col-sm-5">
+                        <input type="text" class="form-control" id="plate" name="plate" placeholder="Ex: ABC1D23" value="{{$car->plate}}">
+                    </div>
                 </div>
-                    <label for="color">Cor:</label>
-                    <input type="text" id="color" name="color" value="{{$car->color}}">
-                <div>
-                <div>
-                    <label for="manufacturing">Ano de Fabricação:</label>
-                    <input type="text" id="manufacturing" name="manufacturing" value="{{$car->manufacturing}}">
+                <div class="form-group row mt-2">
+                    <label for="color" class="col-sm-2 col-form-label" >Cor:</label>
+                    <div class="col-sm-5">
+                        <input type="text" class="form-control" id="color" placeholder="Ex: Preto" name="color" value="{{$car->color}}">
+                    </div>
                 </div>
-                <a href="/car">NEW</a>
-                <input type="hidden" id="id" name="id" value="$car->id">
-                <button type="submit">Salvar</button>
+                <div class="form-group row mt-2">
+                    <label for="manufacturing" class="col-sm-2 col-form-label" >Ano de Fabricação:</label>
+                    <div class="col-sm-5">
+                        <input type="text" class="form-control" id="manufacturing" placeholder="Ex: 2020" name="manufacturing" value="{{$car->manufacturing}}">
+                    </div>
+                </div>
+                <div class="btn-toolbar row">
+                    <input type="hidden" id="id" name="id" value="$car->id">
+                    <div class="btn-group mr-2 mt-2 col-md-1 offset-md-5">
+                        <button type="submit" class="btn btn-success">Salvar</button>
+                    </div>
+                    <div class="btn-group mr-2 mt-2 col-md-1">
+                        <button href="/car" class="btn btn-primary">Novo</button>
+                    </div>
                 </div>
             </div>
         </form>
 
-        <table border="1">
+        <table class="table table-bordered table-striped mt-5">
             <thead>
                 <tr>
-                    <td>Marca</td>
-                    <td>Modelo do Veiculo</td>
-                    <td>Placa</td>
-                    <td>Cor</td>
-                    <td>Ano de Fabricação</td>
+                    <th>Marca</th>
+                    <th>Modelo do Veiculo</th>
+                    <th>Placa</th>
+                    <th>Cor</th>
+                    <th>Ano de Fabricação</th>
                 </tr>
             </thead>
             <tbody>
@@ -55,12 +72,12 @@
                         <td>{{$car->color}}</td>
                         <td>{{$car->manufacturing}}</td>
                         <td> 
-                            <a href="/car/{{ $car-> id }}/edit"> EDIT</a> 
+                            <button href="/car/{{ $car-> id }}/edit" class="btn btn-sm btn-primary"> EDIT</button>  
                         </td>
                         <td> <form action="/car/{{$car->id}}" method="POST">
                                 @csrf
                                 <input type="hidden" name="_method" value="DELETE">
-                                <button type="Submit">DEL</button>
+                                <button type="Submit" class="btn btn-sm btn-danger center">DEL</button>
                             </form>
                         </td>   
 
@@ -68,5 +85,6 @@
                 @endforeach
             </tbody>
         </table>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     </body>
 </html>
